2a3a7db1016da0f31a4c92d434f0c7f3c145a7fb
noahlb123 <noahliguoribills@gmail.com>
2022-02-28 11:59:34 -0500

    Added Ramachandran Plotting

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
+pdb_path = './pdb-files/pdb6s7o.ent'
+
+#Use Bio to get phi psi
 from Bio.PDB.ic_rebuild import structure_rebuild_test
 from Bio.PDB.internal_coords import IC_Chain
 from Bio.PDB.PDBParser import PDBParser
 
-#simple script to get dihedral angles
 parser = PDBParser(PERMISSIVE=1)
-structure = parser.get_structure("placeholder", './pdb-files/pdb6s7o.ent')
+structure = parser.get_structure("placeholder", pdb_path)
 
-print(IC_Chain(structure).ordered_aa_ic_list)
+#phi psi angles
+#print(IC_Chain(structure).ordered_aa_ic_list)
+
+
+#Use RamachanDraw (superset of Bio) to get phi psi and plot
+from RamachanDraw import phi_psi, plot
+
+#phi psi dict
+phi_psi_dict = phi_psi(pdb_path, return_ignored=False)
+
+#plot
+plot(pdb_path, out='ram-plots/plot.png')
